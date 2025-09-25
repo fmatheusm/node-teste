591e0931b2dc3896a4268d7d724789be7c837814
Matheus Martins <matheuss_fernandes@outlook.com>
2025-09-24 23:01:11 -0300

    add autor

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node, ...globals.jest } },
+    rules: {
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      "no-multiple-empty-lines": ["error", { max: 2 }],
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    }
+
+  },
+]);
